f6ff8222ce70ccd1a3015e4932a6bb3167b44056
Percy Leon <percyleon107@gmail.com>
2020-11-15 23:00:13 -0500

    Crud Concluido

--- TableExplorer/TableExplorer/Program.cs
+++ TableExplorer/TableExplorer/Program.cs
@@ -16,25 +16,25 @@ namespace TableExplorer
             CloudTableClient ClienteTablas = CuentaAlamacenamiento.CreateCloudTableClient();
 
             CloudTable Tabla = ClienteTablas.GetTableReference("Clases");
+            Tabla.DeleteIfExists(); /* Eliminar tabla*/
 
-            /* Listar las tablas */
-            //Tabla.CreateIfNotExists();
+            ///* Listar las tablas */
+            //Tabla.CreateIfNotExists(); /* Crea la tabla si no existe */
 
             //var NombreTablas = ClienteTablas.ListTables();
 
             ///* var se le asigna el valor retornado en este caso lo especificaomos pero no es necesario*/
-            //foreach (CloudTable item in NombreTablas) 
+            //foreach (CloudTable item in NombreTablas)
             //{
             //    Console.WriteLine(item.Name);
             //}
 
-            /* Insertar entidades a una tabla */
-
-            //Profesor Profe1 = new Profesor("001", "Profesores");
+            ///* Insertar entidades a una tabla */
+            //Profesor Profe1 = new Profesor("003", "Profesores");
             //Profe1.NombreProfesor = "Percy Leon";
             //Profe1.NombreAsignatura = "Microcontroladores";
 
-            //Profesor Profe2 = new Profesor("002", "Profesores");
+            //Profesor Profe2 = new Profesor("004", "Profesores");
             //Profe2.NombreProfesor = "Victor Leon";
             //Profe2.NombreAsignatura = "Diseño audiovisual";
 
@@ -44,8 +44,62 @@ namespace TableExplorer
             //Tabla.Execute(InsertProfe1);
             //Tabla.Execute(InsertProfe2);
 
-            Console.WriteLine("Entidades insertadas con exito");
-  
+            /* Listar registros de la tabla Clases */
+            //TableQuery<Profesor> Consulta = new TableQuery<Profesor>()
+            //        .Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.GreaterThan, "000"));
+
+            //var ListRegistros = Tabla.ExecuteQuery(Consulta);
+
+            //foreach (Profesor profe in ListRegistros)
+            //{
+            //    Console.WriteLine("{0}, {1}\t{2}\t{3}", profe.PartitionKey, profe.RowKey, profe.NombreProfesor, profe.NombreAsignatura);
+            //}
+
+            ///* Modificar */
+            //TableOperation OperacionModificar = TableOperation.Retrieve<Profesor>("002", "Profesores"); /* Select */
+            //TableResult ResultadoObtenido = Tabla.Execute(OperacionModificar);
+            //Profesor EntidadModificada = (Profesor)ResultadoObtenido.Result;
+
+            ///* Si encuentra el registro */
+            //if (EntidadModificada != null)
+            //{
+            //    EntidadModificada.NombreAsignatura = "Diseño Grafico";
+            //    TableOperation OperacionActualizar = TableOperation.Replace(EntidadModificada);
+            //    Tabla.Execute(OperacionActualizar);
+
+            //    Console.WriteLine("tu reagistro ha sido modificado");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Tu entidad no existe");
+            //}
+
+            /* Eliminar */
+            //TableOperation OperacionModificar = TableOperation.Retrieve<Profesor>("002", "Profesores"); /* Select */
+            //TableResult ResultadoObtenido = Tabla.Execute(OperacionModificar);
+            //Profesor EntidadEliminada = (Profesor)ResultadoObtenido.Result;
+
+            ///* Si encuentra el registro */
+            //if (EntidadEliminada != null)
+            //{
+            //    TableOperation OperacionEliminarr = TableOperation.Delete(EntidadEliminada);
+            //    Tabla.Execute(OperacionEliminarr);
+
+            //    Console.WriteLine("tu reagistro ha sido modificado");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Tu entidad no existe");
+            //}
+
+            //var ListRegistros = Tabla.ExecuteQuery(Consulta);
+
+            //foreach (Profesor profe in ListRegistros)
+            //{
+            //    Console.WriteLine("{0}, {1}\t{2}\t{3}", profe.PartitionKey, profe.RowKey, profe.NombreProfesor, profe.NombreAsignatura);
+            //}
+
+            Console.WriteLine("Lista de profesores obtenidad con exito");
             Console.ReadLine();
         }
     }
