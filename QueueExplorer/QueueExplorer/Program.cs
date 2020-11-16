2e663e59bb4ea1b91c27e846cfd433ffffe8a962
Percy Leon <percyleon107@gmail.com>
2020-11-15 23:17:37 -0500

    Queue

---
+++ QueueExplorer/QueueExplorer/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace QueueExplorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CloudStorageAccount MiCuenta = 
+                CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StringConexion"));
+
+            CloudQueueClient ClienteColas = MiCuenta.CreateCloudQueueClient();
+        }
+    }
+}
